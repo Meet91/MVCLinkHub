24f5bbdd2b543afe93a7b4e2f4851af2a69545cd
Mitesh Patel <Meet91@users.noreply.github.com>
2015-01-21 12:18:49 -0600

    In User's Area,URL Controller and it's view is added.

--- LinkHubUI/LinkHubUI/Areas/User/Controllers/URLController.cs
+++ LinkHubUI/LinkHubUI/Areas/User/Controllers/URLController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace LinkHubUI.Areas.User
+namespace LinkHubUI.Areas.User.Controllers
 {
     public class URLController : Controller
     {
