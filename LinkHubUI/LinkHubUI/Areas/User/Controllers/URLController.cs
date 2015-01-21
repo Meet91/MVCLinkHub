5766811a1cdcfc3e30772ae7fee9524a7066628a
Mitesh Patel <Meet91@users.noreply.github.com>
2015-01-21 12:45:41 -0600

    Controllers and their related actions method are added in all areas.

--- LinkHubUI/LinkHubUI/Areas/User/Controllers/URLController.cs
+++ LinkHubUI/LinkHubUI/Areas/User/Controllers/URLController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BOL;
+
 
 namespace LinkHubUI.Areas.User.Controllers
 {
@@ -11,6 +13,15 @@ namespace LinkHubUI.Areas.User.Controllers
         // GET: User/URL
         public ActionResult Index()
         {
+            LinkHubDbEntities db = new LinkHubDbEntities();
+            ViewBag.CategoryId = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");
+            return View();
+        }
+
+        public ActionResult Create(tbl_Url objUrl)
+        {
+            //LinkHubDbEntities db = new LinkHubDbEntities();
+            //ViewBag.CategoryId = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");
             return View();
         }
     }
