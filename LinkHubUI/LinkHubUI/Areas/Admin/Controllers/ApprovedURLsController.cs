b788db88c1840a86a6fc0f95d1e4301190b3eebc
Mitesh Patel <Meet91@users.noreply.github.com>
2015-01-26 15:27:58 -0600

    ApprovedURLs Controller is modified.

--- LinkHubUI/LinkHubUI/Areas/Admin/Controllers/ApprovedURLsController.cs
+++ LinkHubUI/LinkHubUI/Areas/Admin/Controllers/ApprovedURLsController.cs
@@ -9,9 +9,55 @@ namespace LinkHubUI.Areas.Admin.Controllers
     public class ApprovedURLsController : BaseAdminController
     {
         // GET: Admin/ApprovedURLs
-        public ActionResult Index()
+        public ActionResult Index(string Status)
         {
-            return View();
+
+            ViewBag.Status = (Status == null ? "P" : Status);
+            if(Status==null)
+            {
+                var urls = ObjBs.urlBs.GetAll().Where(x => x.IsApproved == "P").ToList();
+                return View(urls);
+            }
+            else
+            {
+                var urls = ObjBs.urlBs.GetAll().Where(x => x.IsApproved == Status).ToList();
+                return View(urls);
+            }
+        }
+
+        public ActionResult Approve(int id)
+        {
+            try
+            {
+                var myUrl = ObjBs.urlBs.GetById(id);
+                myUrl.IsApproved = "A";
+                ObjBs.urlBs.Update(myUrl);
+                TempData["msg"] = "Approved Successfully.";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = "Approved Failed: " + ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+
+        public ActionResult Reject(int id)
+        {
+            try
+            {
+                var myUrl = ObjBs.urlBs.GetById(id);
+                myUrl.IsApproved = "R";
+                ObjBs.urlBs.Update(myUrl);
+                TempData["msg"] = "Rejected Successfully.";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = "Reject Failed: " + ex.Message;
+                return RedirectToAction("Index");
+            }
         }
     }
 }
