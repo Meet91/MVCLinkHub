27d85dbbe8d4cc455ac18bfce7ef8f6a176f504e
Mitesh Patel <Meet91@users.noreply.github.com>
2015-01-26 12:12:26 -0600

    Url validation class is added.

---
+++ BOL/tbl_UrlValidation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOL
+{
+    public class tbl_UrlValidation
+    {
+
+        [Required]
+        public string UrlTitle { get; set; }
+
+        [Required]
+        [Url]
+        public string Url { get; set; }
+
+        [Required]
+        public string UrlDesc { get; set; }
+    }
+
+    [MetadataType(typeof(tbl_UrlValidation))]
+    public partial class tbl_Url
+    { }
+}
