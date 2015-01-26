cfb4b226941a2278528215f74ed51a8c9115f741
Mitesh Patel <Meet91@users.noreply.github.com>
2015-01-26 13:28:27 -0600

    BaseSecurityController is added.

---
+++ LinkHubUI/LinkHubUI/Areas/Security/Controllers/BaseSecurityController.cs
@@ -0,0 +1,18 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkHubUI.Areas.Security.Controllers
+{
+    public class BaseSecurityController : Controller
+    {
+        protected SecurityBs ObjBs;
+        public BaseSecurityController()
+        {
+            ObjBs = new SecurityBs();
+        }
+    }
+}
