a7710adbc4056f2157f198c3eee73cf4ca42bc0b
Mitesh Patel <Meet91@users.noreply.github.com>
2015-01-26 13:27:54 -0600

    BaseUserController is added.

---
+++ LinkHubUI/LinkHubUI/Areas/User/Controllers/BaseUserController.cs
@@ -0,0 +1,25 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkHubUI.Areas.User.Controllers
+{
+    public class BaseUserController : Controller
+    {
+        protected UserAreaBs ObjBs;
+
+        public BaseUserController()
+        {
+            ObjBs = new UserAreaBs();
+        }
+
+        // GET: User/BaseUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
