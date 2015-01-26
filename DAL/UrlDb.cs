288295412c5fc9ab01cc1890b30f31fc86ed68d5
Mitesh Patel <Meet91@users.noreply.github.com>
2015-01-26 15:42:06 -0600

    Disabling validation on Save.

--- DAL/UrlDb.cs
+++ DAL/UrlDb.cs
@@ -43,6 +43,9 @@ namespace DAL
         public void Update(tbl_Url url)
         {
             db.Entry(url).State = EntityState.Modified;
+            db.Configuration.ValidateOnSaveEnabled = false;
+            Save();
+            db.Configuration.ValidateOnSaveEnabled = true;
         }
 
         public void Save()
