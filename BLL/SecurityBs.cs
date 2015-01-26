d8324fa80989ded531df15e172e668903dc57b4f
Mitesh Patel <Meet91@users.noreply.github.com>
2015-01-26 12:42:37 -0600

    Accessing class through common Securitybs for Security Area is implemented.

---
+++ BLL/SecurityBs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class SecurityBs
+    {
+        public CategoryBs categoryBs { get; set; }
+        public UserBs userBs { get; set; }
+        public UrlBs urlBs { get; set; }
+
+        public SecurityBs()
+        {
+            categoryBs = new CategoryBs();
+            urlBs = new UrlBs();
+            userBs = new UserBs();
+        }
+    }
+}
