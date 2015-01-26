09ad7dea87a855187af0fa47771024d1cf0fa171
Mitesh Patel <Meet91@users.noreply.github.com>
2015-01-26 15:50:50 -0600

    Custom Business rules for tbl_user is added.

---
+++ BOL/tbl_UserValidation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOL
+{
+
+    public class UniqueEmailAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            LinkHubDbEntities db = new LinkHubDbEntities();
+            string userEmail = value.ToString();
+            int count = db.tbl_User.Where(x => x.UserEmail == userEmail).ToList().Count();
+            if (count != 0)
+                return new ValidationResult("UserEmail Already Exists.");
+            return ValidationResult.Success;
+        }
+    }
+
+    public class tbl_UserValidation 
+    {
+        [Required]
+        [EmailAddress]
+        [UniqueEmail]
+        public string UserEmail { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+
+    [MetadataType(typeof(tbl_UserValidation))]
+    public partial class tbl_User
+    { }
+}
