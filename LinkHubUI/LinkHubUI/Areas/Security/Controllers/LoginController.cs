382eccfb7af5f3ca3543afcd22cdfffa84586a31
Mitesh Patel <Meet91@users.noreply.github.com>
2015-01-27 14:48:05 -0600

    Action methods for login and logout is implemented.

--- LinkHubUI/LinkHubUI/Areas/Security/Controllers/LoginController.cs
+++ LinkHubUI/LinkHubUI/Areas/Security/Controllers/LoginController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using BOL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace LinkHubUI.Areas.Security.Controllers
 {
@@ -13,6 +15,35 @@ namespace LinkHubUI.Areas.Security.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignIn(tbl_User user)
+        {
+            try
+            {
+                if (Membership.ValidateUser(user.UserEmail, user.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(user.UserEmail, false);
+                    return RedirectToAction("Index", "Home", new { area = "Common" });
+                }
+                else
+                {
+                    TempData["msg"] = "Login Failed.";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = "Login Failed: "+ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home", new { area = "Common" });
         }
     }
 }
