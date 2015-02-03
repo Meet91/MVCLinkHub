c6d96679122f964c6feb47bfb5d8e6214a407ab6
Mitesh Patel <Meet91@users.noreply.github.com>
2015-02-03 14:45:35 -0600

    InsertQuickURL method is added which performs insertion operation for 2 different entities(tbl_url and tbl_user) using common model "QuickURLSubmitModel"

--- BLL/CommonBs.cs
+++ BLL/CommonBs.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BOL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,19 @@ namespace BLL
 {
     public class CommonBs :BaseBs
     {
-       
+
+        public void InsertQuickURL(QuickURLSubmitModel MyQuickUrl)
+        {
+            tbl_User u = MyQuickUrl.myUser;
+            u.Password = u.ConfirmPassword = "123";
+            u.Role = "U";
+            userBs.Insert(u);
+
+            tbl_Url url = MyQuickUrl.myUrl;
+            url.UserId = u.UserId;
+            url.UrlDesc = url.UrlTitle;
+            url.IsApproved = "P";
+            urlBs.Insert(url);
+        }
     }
 }
