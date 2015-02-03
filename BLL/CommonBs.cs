d9a28928fe3b9845c0283254d7299a1dea14a697
Mitesh Patel <Meet91@users.noreply.github.com>
2015-02-03 15:14:05 -0600

    Transaction for InsertQuickURL method is implemented.

--- BLL/CommonBs.cs
+++ BLL/CommonBs.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
+
 
 namespace BLL
 {
@@ -12,16 +14,29 @@ namespace BLL
 
         public void InsertQuickURL(QuickURLSubmitModel MyQuickUrl)
         {
-            tbl_User u = MyQuickUrl.myUser;
-            u.Password = u.ConfirmPassword = "123";
-            u.Role = "U";
-            userBs.Insert(u);
+            using(TransactionScope trans = new TransactionScope())
+            {
+                try
+                {
+                    tbl_User u = MyQuickUrl.myUser;
+                    u.Password = u.ConfirmPassword = "123";
+                    u.Role = "U";
+                    userBs.Insert(u);
 
-            tbl_Url url = MyQuickUrl.myUrl;
-            url.UserId = u.UserId;
-            url.UrlDesc = url.UrlTitle;
-            url.IsApproved = "P";
-            urlBs.Insert(url);
+                    tbl_Url url = MyQuickUrl.myUrl;
+                    url.UserId = u.UserId;
+                    url.UrlDesc = url.UrlTitle;
+                    url.IsApproved = "P";
+                    urlBs.Insert(url);
+
+                    trans.Complete();
+                }catch(Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+
+            
         }
     }
 }
