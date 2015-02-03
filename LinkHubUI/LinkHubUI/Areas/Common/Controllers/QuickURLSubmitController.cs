d20e2c3de7dda9d56348958c4429eb5ff4ab6800
Mitesh Patel <Meet91@users.noreply.github.com>
2015-02-03 14:50:58 -0600

    QuickURLSubmitController is used to quickly submit any url details without going through any registration process.Here two models(tbl_user and tbl_url) are bind together to a single common view.

---
+++ LinkHubUI/LinkHubUI/Areas/Common/Controllers/QuickURLSubmitController.cs
@@ -0,0 +1,49 @@
+﻿using BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkHubUI.Areas.Common.Controllers
+{
+    [AllowAnonymous]
+    public class QuickURLSubmitController : BaseCommonController
+    {
+        // GET: Common/QuickURLSubmit
+        public ActionResult Index()
+        {
+            ViewBag.CategoryId = new SelectList(ObjBs.categoryBs.GetAll().ToList(), "CategoryId", "CategoryName");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(QuickURLSubmitModel MyQuickUrl )
+        {
+            try
+            {
+                
+                ModelState.Remove("myUser.Password");
+                ModelState.Remove("myUser.ConfirmPassword");
+                ModelState.Remove("myUrl.UrlDesc");
+
+                if(ModelState.IsValid)
+                {
+                    ObjBs.InsertQuickURL(MyQuickUrl);
+                    TempData["msg"] = "Created Successfully.";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.CategoryId = new SelectList(ObjBs.categoryBs.GetAll().ToList(), "CategoryId", "CategoryName");
+                    return View();
+                }
+            }catch(Exception ex)
+            {
+                TempData["msg"] = "Create Failed :"+ex.Message;
+                return RedirectToAction("Index");
+            }
+
+        }
+    }
+}
