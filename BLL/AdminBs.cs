a79a1d62a3321179456a16fdcf6321282f0ab7e9
Mitesh Patel <Meet91@users.noreply.github.com>
2015-02-03 17:05:58 -0600

    ApproveOrReject method to urls is implemented.

--- BLL/AdminBs.cs
+++ BLL/AdminBs.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace BLL
 {
    public class AdminBs : BaseBs
     {
-        
+
+        public void ApproveOrReject(List<int> Ids, string Status)
+        {
+            using (TransactionScope tran = new TransactionScope())
+            {
+                try
+                {
+
+                    foreach (var item in Ids)
+                    {
+                        var myurl = urlBs.GetById(item);
+                        myurl.IsApproved = Status;
+                        urlBs.Update(myurl);
+                    }
+
+                    tran.Complete();
+
+
+                }catch(Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
     }
 }
