325727b12685955db62a04ac545b1c0f6c966035
Mitesh Patel <Meet91@users.noreply.github.com>
2015-02-04 13:51:48 -0600

    Registeration method for googleclient is implemented.

--- LinkHubUI/LinkHubUI/Global.asax.cs
+++ LinkHubUI/LinkHubUI/Global.asax.cs
@@ -4,16 +4,31 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Microsoft.Web.WebPages.OAuth;
 
 namespace LinkHubUI
 {
+
+    public class OAuthConfig
+    {
+        public static void RegisterProviders()
+        {
+            OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+
     public class MvcApplication : System.Web.HttpApplication
     {
+
+       
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             GlobalFilters.Filters.Add(new AuthorizeAttribute());
+
+            OAuthConfig.RegisterProviders();
         }
     }
 }
